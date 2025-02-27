38b84f784276b985df14d48c644d1ec910c6dd9f
AlexRua18 <diomer.rua.r@gmail.com>
2025-02-27 11:12:55 -0500

    Se crea la estructura del proyecto

---
+++ src/main/java/com/example/PedidosApp/modelo/Entrega.java
@@ -0,0 +1,4 @@
+package com.example.PedidosApp.modelo;
+
+public class Entrega {
+}
---
+++ src/main/java/com/example/PedidosApp/modelo/Usuario.java
@@ -0,0 +1,8 @@
+package com.example.PedidosApp.modelo;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.Table;
+
+public class Usuario {
+
+}
