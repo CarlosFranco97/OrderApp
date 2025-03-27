f001851d9de5e08561e7dbd0369eddb2090506ce
AlexRua18 <diomer.rua.r@gmail.com>
2025-03-27 12:45:18 -0500

    Se genera el servicio del usuario

--- src/main/java/com/example/OrderApp/repository/IUserRepository.java
+++ src/main/java/com/example/OrderApp/repository/IUserRepository.java
@@ -7,5 +7,4 @@ import org.springframework.stereotype.Repository;
 @Repository
 public interface IUserRepository extends JpaRepository<User, Integer> {
     //dentro del repo van las consultas personalizadas
-
 }
--- src/main/java/com/example/OrderApp/services/UserService.java
+++ src/main/java/com/example/OrderApp/services/UserService.java
@@ -1,13 +1,86 @@
 package com.example.OrderApp.services;
 
+import com.example.OrderApp.models.User;
 import com.example.OrderApp.repository.IUserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
 
 @Service
 public class UserService {
     @Autowired
     IUserRepository repository;
 
+    //Listar los metodos que activaran
+    //Las consultas en la BD
+
+    //Guardar
+    public User createUser(User user) throws Exception {
+        try {
+            //validar los datos de entrada
+            return this.repository.save(user);
+        } catch(Exception error){
+            throw new Exception(error.getMessage());
+        }
+    }
+
+
+    //buscar todos los registros
+    public List<User> getAllUser() throws Exception{
+        try {
+            return this.repository.findAll();
+        } catch (Exception error) {
+            throw new Exception(error.getMessage());
+        }
+    }
+
+    //buscar por id
+    public Optional<User> getUserById(Integer id) throws Exception {
+        try {
+            if (this.repository.findById(id).isPresent()) {
+                return this.repository.findById(id);
+
+            } else {
+                throw new Exception("El usuario no fue encontrado en la base de datos");
+            }
+        } catch (Exception error) {
+            throw new Exception(error.getMessage());
+        }
+    }
+
+    //modificar por id
+    public User modifyUser(Integer id, User user) throws Exception{
+        try {
+            Optional<User> searchedUser = this.repository.findById(id);
+            if(searchedUser.isPresent()){
+                searchedUser.get().setUserPhone(user.getUserPhone());
+                searchedUser.get().setUserEmail(user.getUserEmail());
+                return this.repository.save(user);
+            } else {
+                throw new Exception("Usuario no encontrado");
+            }
+
+        } catch (Exception error) {
+            throw new Exception(error.getMessage());
+        }
+    }
+
+    //eliminar por id
+
+    public boolean deleteUserById(Integer id) throws Exception{
+        try {
+            Optional<User> searchedUser = this.repository.findById(id);
+            if(searchedUser.isPresent()){
+                this.repository.deleteById(id);
+                return true;
+            } else {
+                throw new Exception("Usuario no encontrado");
+            }
+        } catch(Exception error) {
+            throw new Exception(error.getMessage());
+        }
+    }
 
 }
