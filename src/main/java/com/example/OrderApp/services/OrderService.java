8a28cfe48d05e065fafa8ba88252dc2b4749f2ec
CarlosFranco97 <127879068+CarlosFranco97@users.noreply.github.com>
2025-04-03 09:49:02 -0500

    Se empieza a trabajr en el servicio de pedido

--- src/main/java/com/example/OrderApp/services/OrderService.java
+++ src/main/java/com/example/OrderApp/services/OrderService.java
@@ -1,11 +1,21 @@
 package com.example.OrderApp.services;
 
+import com.example.OrderApp.models.Order;
 import com.example.OrderApp.repository.IOrderRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @Service
 public class OrderService {
     @Autowired
     IOrderRepository repository;
+    public List<Order> getAllOrders() throws Exception{
+        try {
+            return this.repository.findAll();
+        } catch (Exception error){
+            throw new Exception(error);
+        }
+    }
 }
